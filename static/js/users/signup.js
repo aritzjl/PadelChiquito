160f397800c7cf859df2244ccca70b6a58cf236b
Angel Jimenez <angeljimenezabreu2@gmail.com>
2023-12-10 17:19:48 -0400

    Users - done

--- static/js/users/signup.js
+++ static/js/users/signup.js
@@ -1,20 +1,3 @@
-const errorElement = document.getElementById('error');
-console.log('keloke')
-if (errorElement && typeof errorElement.value === 'string' && errorElement.value.trim() !== '') {
-  const errorMessage = errorElement.value;
-  if (typeof Swal === 'undefined') {
-    console.error('SweetAlert2 library is not loaded');
-  } else {
-    Swal.fire({
-      icon: 'error',
-      title: errorMessage,
-      showConfirmButton: true,
-      timer: 20000,
-      confirmButtonColor: '#d1ff4f'
-    });
-  }
-}
-
 const inputDate = document.getElementById('fecha');
 
 inputDate.addEventListener('input', () => {
