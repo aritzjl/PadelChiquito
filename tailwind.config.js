3e528c3af86b14a19d275689ff853289c64a7490
Angel Jimenez <angeljimenezabreu2@gmail.com>
2023-12-19 13:34:29 -0400

    he agregado colores personalizados a tailwind en vez de usar valores arbitrarios

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'electric-lime': '#ccff00',
+        'lime-zest': '#d9ff00',
+      }
+    },
   },
   plugins: [],
 }
