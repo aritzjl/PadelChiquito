80bae584b17237af3c555b7ba39cc49b70d7451e
root <root@srv437859.hstgr.cloud>
2024-01-10 16:32:50 +0000

    Cambiado contenido tabla, agregada URL a tienda en el html

--- padelchiquito/settings.py
+++ padelchiquito/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    #'django.contrib.sites',
     
     #Started apps
     'apps.comparador',
