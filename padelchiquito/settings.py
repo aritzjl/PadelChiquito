bfd073dfca813836d672a75b4ce778882f16ba6b
root <root@srv437859.hstgr.cloud>
2024-01-29 19:44:26 +0000

    Arreglado trusted origins y hosts para https

--- padelchiquito/settings.py
+++ padelchiquito/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = 'django-insecure-+^m)ej)i9tvh#e1fdvwelwd1i$*(u%a!+a35z$x*q(k9im2k6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.16.131.191','www.padelchiquito.com','padelchiquito.com','127.0.0.1' ]
+ALLOWED_HOSTS = ['178.16.131.191','www.padelchiquito.com','padelchiquito.com','127.0.0.1','https://www.padelchiquito.com']
+
+CSRF_TRUSTED_ORIGINS = [
+'https://www.padelchiquito.com',   
+]
 
 # Ruta donde se almacenarán las imágenes cargadas
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
