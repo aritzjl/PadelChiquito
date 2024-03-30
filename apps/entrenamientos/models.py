6ea2ea8d146225d5556e1c4fa6c6b5697f64b63e
iBeltz-py <aritzzjl@gmail.com>
2024-03-30 11:56:23 +0100

    Arreglado google an, y temrinado backend blog y entrenamientos

--- apps/entrenamientos/models.py
+++ apps/entrenamientos/models.py
@@ -7,5 +7,7 @@ class Entrenamiento(models.Model):
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=8, decimal_places=2)
+    codigoDescuento = models.CharField(max_length=30, blank=True, null=True)
+    precioDescuento = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)    
     imagen_portada = models.ImageField(upload_to='entrenamientos/')
     url = models.URLField()
