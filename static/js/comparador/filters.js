3a421e001f1ac4dbe07e37b9515ee14641551131
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-06-15 12:03:01 -0400

    feat: add filter by level

--- static/js/comparador/filters.js
+++ static/js/comparador/filters.js
@@ -2,11 +2,21 @@
 const filterByMaxPriceForm = document.getElementById('filterByMaxPriceForm');
 const filterByOrderForm = document.getElementById('filterByOrdenForm');
 const filterByBrandForm = document.getElementById('filterByBrandForm');
-const orden = document.getElementById('orden');
+const filterByLevelForm = document.getElementById('filterByLevelForm');
+
+let ordenSelect = document.getElementById('ordenSelect');
+let levelSelect = document.getElementById('levelSelect');
 let filterRangePrice = document.getElementById('filter_price_max');
 let maxPrice = document.getElementById('filter_price_max_number');
+const caracteristics = document.getElementById('caracteristics');
 
 const checkboxes = filterByBrandForm.querySelectorAll('input[type="checkbox"]');
+const dropdowns = caracteristics.querySelectorAll('div > div:nth-child(1)');
+
+const dropdownArrows = {
+  arrowDown: document.getElementById('arrow-down'),
+  arrowUp: document.getElementById('arrow-up')
+}
 
 function sendForm(form) {
   form.submit();
@@ -21,6 +31,10 @@ function handleOrderChange() {
   sendForm(filterByOrderForm);
 }
 
+function handleLevelChange() {
+  sendForm(filterByLevelForm);
+}
+
 function handleCheckboxChange(event) {
   const { parentElement, checked } = event.target;
   parentElement.classList.toggle('bg-black', checked);
@@ -30,9 +44,18 @@ function handleCheckboxChange(event) {
 }
 
 // Event Listeners and main logic
-orden.addEventListener('change', handleOrderChange);
+ordenSelect.addEventListener('change', handleOrderChange);
+levelSelect.addEventListener('change', handleLevelChange);
 filterRangePrice.addEventListener('input', handlePriceRangeChange);
 
 checkboxes.forEach(checkbox => {
   checkbox.addEventListener('change', handleCheckboxChange);
+});
+
+dropdowns.forEach(dropdown => {
+  dropdown.addEventListener('click', () => {
+    filterByLevelForm.classList.toggle('hidden');
+    dropdownArrows.arrowDown.classList.toggle('hidden');
+    dropdownArrows.arrowUp.classList.toggle('hidden');
+  })
 });
