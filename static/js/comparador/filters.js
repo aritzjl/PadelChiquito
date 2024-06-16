def838be26d14d7b6fd52d49d8b2e393dc5b6c50
Angel Jimenez <angeljimenezabreu2@gmail.com>
2024-06-15 21:23:46 -0400

    feat: add filter by volley
    refactor: use function instead of repeating code for update and submit range inputs

--- static/js/comparador/filters.js
+++ static/js/comparador/filters.js
@@ -4,14 +4,17 @@ const filterByMaxPriceForm = document.getElementById('filterByMaxPriceForm');
 // Nota padelchiquito
 const filterByBaseline = document.getElementById('filterByBaseline');
 const filterByWallSmash = document.getElementById('filterByWallSmash');
+const filterByVolley = document.getElementById('filterByVolley');
 
 let rangePrice = document.getElementById('filter_price_max');
 let rangeBaseline = document.getElementById('rangeBaseline');
 let rangeWallSmash = document.getElementById('rangeWallSmash');
+let rangeVolley = document.getElementById('rangeVolley');
 
 let maxPrice = document.getElementById('filter_price_max_number');
 let minBaseline = document.getElementById('fondo_pista_min_number');
 let minWallSmash = document.getElementById('bajada_pared_min_number');
+let minVolley = document.getElementById('volea_min_number');
 
 const notaPadelChiquito = document.getElementById('notaPadelChiquito');
 const dropdownsNotaPadelChiquito = notaPadelChiquito.querySelectorAll('div > div > div:nth-child(1)');
@@ -67,6 +70,13 @@ function toggleDropdown(dropdowns, shouldToggleFlex) {
   });
 };
 
+function updateAndSubmitForm (rangeElement, displayElement, form) {
+  rangeElement.addEventListener('change', () => {
+    displayElement.textContent = rangeElement.value + (rangeElement === rangePrice ? ' \u20AC' : '');
+    sendForm(form);
+  });
+};
+
 // Event Listeners and main logic
 handleInputChange(ordenSelect, filterByOrderForm);
 handleInputChange(levelSelect, filterByLevelForm);
@@ -77,20 +87,10 @@ handleInputChange(shapeSelect, filterByShapeForm);
 toggleDropdown(dropdownsCaracteristics, false);
 toggleDropdown(dropdownsNotaPadelChiquito, true);
 
-rangePrice.addEventListener('change', () => {
-  maxPrice.textContent = `${rangePrice.value} \u20AC`;
-  sendForm(filterByMaxPriceForm);
-});
-
-rangeBaseline.addEventListener('change', () => {
-  minBaseline.textContent = rangeBaseline.value;
-  sendForm(filterByBaseline);
-});
-
-rangeWallSmash.addEventListener('change', () => {
-  minWallSmash.textContent = rangeWallSmash.value;
-  sendForm(filterByWallSmash);
-});
+updateAndSubmitForm(rangePrice, maxPrice, filterByMaxPriceForm);
+updateAndSubmitForm(rangeBaseline, minBaseline, filterByBaseline);
+updateAndSubmitForm(rangeWallSmash, minWallSmash, filterByWallSmash);
+updateAndSubmitForm(rangeVolley, minVolley, filterByVolley);
 
 checkboxes.forEach(checkbox => {
   checkbox.addEventListener('change', handleCheckboxChange);
