ebb29ed4eb14825a2b690ec7271da7565807c67a
aritzjl <aritzzjl@gmail.com>
2024-06-30 17:19:08 +0200

    Ahora los anuncios cad aimagen tiene su propio url, y hay anuncios entre las palas

--- apps/bannerspubli/models.py
+++ apps/bannerspubli/models.py
@@ -4,13 +4,17 @@ from django.db import models
 class ContenidoPublicitario(models.Model):
     
     nombre = models.CharField(max_length=255, help_text='Nombre de la campaña publiciaria, por ejemplo: "Black Friday 2021"')
-    url = models.URLField(blank=True, null=True)
     inicio_1 = models.ImageField(upload_to='publi', null=True, blank=True, help_text='Tamaño recomendado: 1920x1080')
+    inicio_1_url = models.URLField(blank=True, null=True)
     inicio_2 = models.ImageField(upload_to='publi', null=True, blank=True, help_text='Tamaño recomendado: 1920x1080')
+    inicio_2_url = models.URLField(blank=True, null=True)
     inicio_3 = models.ImageField(upload_to='publi', null=True, blank=True, help_text='Tamaño recomendado: 1920x1080')
+    inicio_3_url = models.URLField(blank=True, null=True)
     
     filtros_1 = models.ImageField(upload_to='publi', null=True, blank=True, help_text='Tamaño recomendado: 1920x1080')
+    filtros_1_url = models.URLField(blank=True, null=True)
     filtros_2 = models.ImageField(upload_to='publi', null=True, blank=True, help_text='Tamaño recomendado: 1920x1080')
+    filtros_2_url = models.URLField(blank=True, null=True)
     
     activo = models.BooleanField(default=False)
     fecha_inicio = models.DateField(auto_now_add=True)
